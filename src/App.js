4af1e4e2d6abd9e798f3039777d361cededf14a4
RadonZ <supachai27102000@gmail.com>
2024-04-29 11:24:01 +0700

    getdata

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import InfiniteScroll from "react-infinite-scroll-component"
+import  axios  from "axios"
+import { useEffect, useState } from "react"
+
+function ScrollDetail(){
+  const [result,setResult] = useState('');
+  const [limit,setLimit] = useState(20);
+
+  
+
+  useEffect(() => {
+    axios
+    .get("http://api.sampleapis.com/codingresources/codingResources")
+    .then((response)=>{
+      const limitingItem = response.data.slice(0,limit)
+      setLimit(limitingItem)
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
+  },[limit])
+
+  
+}
+
+function Card(){
+
+}
+
+function Loader(){
+
+}
+
 function App() {
   return (
-    <div>
-      
+    <div className="main-screen">
+      <ScrollDetail/>
     </div>
   )
 }
